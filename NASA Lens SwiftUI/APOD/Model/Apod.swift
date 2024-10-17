1eb8fe5a1c1e741560c2bbf011719c9e347d0cbf
John Motta <gabriel_motta31@hotmail.com>
2024-10-17 13:57:20 -0300

    Create Apod model

--- NASA Lens SwiftUI/APOD/Model/Apod.swift
+++ NASA Lens SwiftUI/APOD/Model/Apod.swift
@@ -1,8 +1,28 @@
 //
-//  APOD.swift
+//  Apod.swift
 //  NASA Lens SwiftUI
 //
 //  Created by John Motta on 17/10/24.
 //
 
 import Foundation
+
+struct Apod: Codable {
+    let copyright: String?
+    let date: String
+    let explanation: String
+    let mediaType: String
+    let serviceVersion: String
+    let title: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case copyright
+        case date
+        case explanation
+        case mediaType = "media_type"
+        case serviceVersion = "service_version"
+        case title
+        case url
+    }
+}
