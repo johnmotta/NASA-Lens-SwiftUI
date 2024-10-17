44535f0d3fed9fee3451b22d0074d46b3a81c839
John Motta <gabriel_motta31@hotmail.com>
2024-10-17 13:32:49 -0300

    first commit

---
+++ NASA Lens SwiftUI/NASA_Lens_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_Lens_SwiftUIApp.swift
+//  NASA Lens SwiftUI
+//
+//  Created by John Motta on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NASA_Lens_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
