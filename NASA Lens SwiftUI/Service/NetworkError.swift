66345679630d3ed1f8cd2ea802b4a3f068f6fe2f
John Motta <gabriel_motta31@hotmail.com>
2024-10-17 13:58:38 -0300

    Create Service

--- NASA Lens SwiftUI/Service/NetworkError.swift
+++ NASA Lens SwiftUI/Service/NetworkError.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case invalidURL(String)
+    case noData
+    case invalidResponse
+    case decodingError(Error)
+    case apiError(Int, String)
+    case networkFailure(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL(let url):
+            return "URL inválida -> \(url)."
+        case .noData:
+            return "Dados não recebidos da API."
+        case .invalidResponse:
+            return "Resposta inválida da API."
+        case .decodingError(let error):
+            return "Decodificação falhou: \(error.localizedDescription)"
+        case .networkFailure(let error):
+            return "Falha na rede: \(error.localizedDescription)"
+        case .apiError(let status, let error):
+            return "Error na API. Status code: \(status), error: \(error)"
+        }
+    }
+}
--- NASA Lens SwiftUI/Service/ServiceManager.swift
+++ NASA Lens SwiftUI/Service/ServiceManager.swift
@@ -6,3 +6,133 @@
 //
 
 import Foundation
+
+protocol NEOServiceProtocol {
+    func fetchLoadAstronomyPictures(completion: @escaping (Result<Apod, NetworkError>) -> Void)
+    func fetchData(completion: @escaping (Result<[MarsRoverPhoto], NetworkError>) -> Void)
+    func fetchNEOData(startDate: String, endDate: String, completion: @escaping (Result<NEOResponse, NetworkError>) -> Void)
+}
+
+class ServiceManager: NEOServiceProtocol {
+    static let shared = ServiceManager()
+    
+    func fetchLoadAstronomyPictures(completion: @escaping (Result<Apod, NetworkError>) -> Void) {
+        let urlString = "https://api.nasa.gov/planetary/apod?api_key=\(Constants.API)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidURL(urlString)))
+            return
+        }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { data, _, error in
+            if let error {
+                completion(.failure(.networkFailure(error)))
+                return
+            }
+            
+            guard let data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(Apod.self, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }
+        task.resume()
+    }
+    
+    func fetchData(completion: @escaping (Result<[MarsRoverPhoto], NetworkError>) -> Void) {
+        let urlString = "\(Constants.baseURL)mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=\(Constants.API)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidURL(urlString)))
+            return
+        }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { data, _, error in
+            if let error {
+                completion(.failure(.networkFailure(error)))
+                return
+            }
+            
+            guard let data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .useDefaultKeys
+                let response = try decoder.decode(MarsRoverPhotosResponse.self, from: data)
+                completion(.success(response.photos))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }
+        task.resume()
+    }
+        
+    func fetchNEOData(startDate: String, endDate: String, completion: @escaping (Result<NEOResponse, NetworkError>) -> Void) {
+        let baseURL = "https://api.nasa.gov/neo/rest/v1/feed"
+        let apiKey = Constants.API
+        guard var urlComponents = URLComponents(string: baseURL) else {
+            completion(.failure(.invalidURL(baseURL)))
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "start_date", value: startDate),
+            URLQueryItem(name: "end_date", value: endDate),
+            URLQueryItem(name: "api_key", value: apiKey)
+        ]
+        
+        guard let url = urlComponents.url else {
+            completion(.failure(.invalidURL(baseURL)))
+            return
+        }
+                
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error {
+                completion(.failure(.networkFailure(error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.networkFailure(NSError(domain: "Invalid response", code: 0, userInfo: nil))))
+                return
+            }
+            
+            
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(.apiError(httpResponse.statusCode, "API Error")))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let neoResponse = try decoder.decode(NEOResponse.self, from: data)
+                completion(.success(neoResponse))
+            } catch {
+                print("Decoding error: \(error.localizedDescription)")
+                if let decodingError = error as? DecodingError {
+                    print("Decoding Error Details: \(decodingError)")
+                }
+                completion(.failure(.decodingError(error)))
+            }
+        }
+        task.resume()
+    }
+}
